6fe2ccd312c72bbc2c66a314415d96e4f5bc2b22
Daksh-Sharma-02 <159519639+Daksh-Sharma-02@users.noreply.github.com>
2024-02-15 12:02:32 +0530

    Update evenandoddusingfunction.c

--- evenandoddusingfunction.c
+++ evenandoddusingfunction.c
@@ -17,27 +17,3 @@ int main(){
     evenodd(x);
 }
 
-// #include<stdio.h>
-// int evenodd(int x);
-
-// int main(){
-//     int x;
-//     printf("Enter x :");
-//     scanf("%d",&x);
-//      if(x%2==0){
-//         printf("%d is Even ",x);
-//     }
-//     if(x%2!=0){
-//         printf("%d is odd ",x);
-//     }
-
-// }
-// int evenodd(int x){
-//     if(x%2==0){
-//         printf("%d is Even ",x);
-//     }
-//     if(x%2!=0){
-//         printf("%d is odd ",x);
-//     }
-    
-// }
